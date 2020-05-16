b9e7a69902f1519f301e257585ed37dfa1e2e36e
nidhiraj98 <nidhiraj9899@gmail.com>
2020-05-16 14:00:15 +0530

    Decoder for (15,5) BCH Code

--- computeSyndrome.py
+++ computeSyndrome.py
@@ -3,7 +3,7 @@ n = 4   #GF(2^4)
 t = 3   #Error Correcting Capability
 GF = {}
 
-r = [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0] #Received Vector
+r = [1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0] #Received Vector
 
 def generateField(n):
     GF[0] = 0b1
@@ -13,8 +13,8 @@ def generateField(n):
         GF[i] = GF[i - n] ^ GF[i - n + 1]   #Primitive Polynomial = x^4 = x + 1
 
 def inverse(num):
-    num = [str(i) for i in num]
-    num = int("".join(num), 2)
+    # num = [str(i) for i in num]
+    # num = int("".join(num), 2)
     for key, val in GF.items():
         if val == num:
             inv_key = (15 - key) % 15
@@ -24,27 +24,17 @@ def inverse(num):
     
 
 def fieldMul(a, b):
-    p = 0b0
-    for _ in range(n):
-        if b % 2 == 1:
-            p ^= a
-        b >>= 1
-        a <<= 1
-    # print(p)
-    return reducedForm(p)
-
-def reducedForm(num):
-    if len(bin(num)[2: ]) <= 4:
-        return num
-    numGF = num & 0b1111
-    binArr = list(bin(num)[2: ])
-    binArr = binArr[0: len(binArr) - 4]
-    binArr.reverse()
-    for i in range(n, n + len(binArr)):
-        if(binArr[n - i] == '1'):
-            numGF ^= GF[i]
-    # print("Field Mul = ", numGF)
-    return numGF
+    if a == 0 or b == 0:
+        return 0
+    # print(a, b)
+    for key, val in GF.items():
+        if val == a:
+            alpha_a = key
+        if val == b:
+            alpha_b = key
+    p = GF[(alpha_a + alpha_b) % (2**n - 1)]
+    # print("Field Mul = ", p)
+    return p
 
 def computeSyndrome(r, x):
     h = []
@@ -58,57 +48,84 @@ def computeSyndrome(r, x):
     return syndrome
 
 def main():
-    generateField(n)
-    # print(fieldMul(6, 12))
-    syndromes = []
-    for i in range(2*t):
-        syndromes.append(computeSyndrome(r, i + 1))
-    print(syndromes)
+    try:
+        generateField(n)
+        syndromes = []
+        for i in range(2*t):
+            syndromes.append(computeSyndrome(r, i + 1))
+        print(syndromes)
 
-    errLocTable = {
-        'mu': [-0.5] + [i for i in range(0, t + 1)],
-        'sigma': [[1], [1]],
-        'd': [[1], [syndromes[0]]],
-        'l': [0, 0],
-        'diff': [-1, 0]
-    }
+        errLocTable = {
+            'mu': [-0.5] + [i for i in range(0, t + 1)],
+            'sigma': [[1], [1]],
+            'd': [[1], [syndromes[0]]],
+            'l': [0, 0],
+            'diff': [-1, 0]
+        }
 
-    for i in range(2, t + 1):
-        mu = errLocTable['mu'][i - 1]
-        # print("mu = ", mu)
-        if errLocTable['d'][i - 1] == 0:
-            sigma = errLocTable['sigma'][i - 1]
-        else:
-            p = errLocTable['mu'][errLocTable['diff'].index(max(errLocTable['diff'][:i - 1]))]
-            # print("p = ", p)
-            d_u = errLocTable['d'][i - 1]
-            d_u = [str(j) for j in d_u]
-            d_u = int("".join(d_u), 2)
-            # print(d_u)
-            d_p_inv = inverse(errLocTable['d'][errLocTable['mu'].index(p)])
-            # print(d_p_inv)
-            sigma_p = errLocTable['sigma'][errLocTable['mu'].index(p)]
-            # print(sigma_p)
-            big_term = list(np.polymul([fieldMul(d_u, d_p_inv)], sigma_p)) + [0 for _ in range(int(2 * (mu - p)))]
-            # print(big_term)
-            sigma = list(np.polyadd(big_term, errLocTable['sigma'][i - 1])) #add x^2*(mu - p) #dry run here
-        l = len(sigma) - 1
-        diff = 2 * errLocTable['mu'][i] - l
-        x = len(sigma)
-        d = list(bin(syndromes[2 * mu + 3 - 1])[2:])
-        d = [int(j) for j in d]
-        for j in range(1, x):
-            syn = list(bin(syndromes[2 * mu + 3 - 1 - l])[2: ])
-            syn = [int(k) for k in syn]
-            d = np.polyadd(d, np.polymul(syn, [sigma[x - j]]))
-            d = [int(k % 2) for k in d]
-        errLocTable['sigma'].append(sigma)
-        errLocTable['d'].append(d)
-        errLocTable['l'].append(l)
-        errLocTable['diff'].append(diff)
-    errLocPoly = errLocTable['sigma'][t]
-    print(errLocPoly)
+        for i in range(2, t + 2):
+            mu = errLocTable['mu'][i - 1]
 
-    # print(errLocTable)
+            if errLocTable['d'][i - 1] == 0: #Compute sigma
+                sigma = errLocTable['sigma'][i - 1]
+            else:
+                p = errLocTable['mu'][errLocTable['diff'].index(max(errLocTable['diff'][:i - 1]))]
+                # print("p = ", p)
+                d_u = errLocTable['d'][i - 1]
+                d_u = [str(j) for j in d_u]
+                d_u = int("".join(d_u), 2)
+                # print("du = ", d_u)
+                d_p_inv = errLocTable['d'][errLocTable['mu'].index(p)]
+                d_p_inv = [str(j) for j in d_p_inv]
+                d_p_inv = inverse(int("".join(d_p_inv), 2))
+                # print("dp = ", d_p_inv)
+                sigma_p = errLocTable['sigma'][errLocTable['mu'].index(p)]
+                # print("sigma_p = ", sigma_p)
+                big_term = list(np.polymul([fieldMul(d_u, d_p_inv)], sigma_p)) + [0 for _ in range(int(2 * (mu - p)))]
+                # print("bigterm = ", big_term)
+                sigma_u = errLocTable['sigma'][i - 1]
+                x = len(big_term) - len(sigma_u)
+                sigma_u = [0 for _ in range(x)] + sigma_u
+                sigma = list(np.bitwise_xor(big_term, sigma_u))
+
+            l = len(sigma) - 1
+            diff = 2 * errLocTable['mu'][i] - l
+
+            errLocTable['sigma'].append(sigma)
+            errLocTable['l'].append(l)
+            errLocTable['diff'].append(diff)
+
+            if(i == t + 1):
+                break
+            x = len(sigma)
+            d = list(bin(syndromes[2 * mu + 3 - 1])[2:]) #initialize d
+            d = [int(j) for j in d]
+            for j in range(1, x):   #compute d
+                syn = list(bin(syndromes[2 * mu + 3 - 1 - l])[2: ])
+                syn = [int(k) for k in syn]
+                d = np.polyadd(d, np.polymul(syn, [sigma[x - j]]))
+                d = [int(k % 2) for k in d]
+            errLocTable['d'].append(d)
+
+        # print(errLocTable)
+
+        errLocPoly = errLocTable['sigma'][t + 1]
+        # print(errLocPoly)
+        errLocPoly.reverse()
+        errLoc = []
+        for i in range(0, 2**n - 1):
+            sum = 0
+            for j in range(0, len(errLocPoly)):
+                # print("i, j = ", i, j)
+                sum ^= fieldMul(errLocPoly[j], GF[(i * j) % (2**n - 1)])
+            if sum == 0:
+                errLoc.append((2**n - 1) - i)
+
+        print(errLoc)
+        for i in errLoc:
+            r[i] ^= 1
+        print("Message Sent: ", r)
+    except:
+        print("Message corrupted. Request Retransmission")
 if __name__ == "__main__":
     main()
